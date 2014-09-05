bb6fed8db82efacfadf8bbee887eef245a80815a
lukemetz <luke.s.metz@gmail.com>
2014-09-05 10:25:14 -0400

    fixed style warnings

--- src/ffi.rs
+++ src/ffi.rs
@@ -51,104 +51,80 @@ extern {
 }
 
 /// Trait to allow interaction with the python interpreter.
+#[allow(bad_style)]
 pub trait PythonCAPI {
-  #[allow(non_snake_case_functions)]
   unsafe fn Py_Initialize(&self) {
     Py_Initialize();
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn Py_Finalize(&self) {
     Py_Finalize();
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyImport_ImportModule(&self, name : *const c_char) -> *mut PyObjectRaw {
     PyImport_ImportModule(name)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyInt_FromLong(&self, ival : c_long) -> *mut PyObjectRaw {
     PyInt_FromLong(ival)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyInt_AsLong(&self, obj : *mut PyObjectRaw) -> c_long {
     PyInt_AsLong(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyFloat_FromDouble(&self, value : c_double) -> *mut PyObjectRaw {
     PyFloat_FromDouble(value)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyFloat_AsDouble(&self, obj : *mut PyObjectRaw) -> c_double {
     PyFloat_AsDouble(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyTuple_New(&self, size : size_t) -> *mut PyObjectRaw {
     PyTuple_New(size)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyTuple_GetItem(&self, tuple : *mut PyObjectRaw, pos : size_t) -> *mut PyObjectRaw {
     PyTuple_GetItem(tuple, pos)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyTuple_SetItem(&self, tuple : *mut PyObjectRaw, pos : size_t, o : *mut PyObjectRaw) {
     PyTuple_SetItem(tuple, pos, o)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyTuple_Size(&self, tuple : *mut PyObjectRaw) -> c_long {
     PyTuple_Size(tuple)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn Py_IncRef(&self, obj: *mut PyObjectRaw) {
     Py_IncRef(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn Py_DecRef(&self, obj: *mut PyObjectRaw) {
     Py_DecRef(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyFloat_Check(&self, obj : *mut PyObjectRaw) -> c_long {
     RPyFloat_Check(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyFloat_CheckExact(&self, obj : *mut PyObjectRaw) -> c_long {
     RPyFloat_CheckExact(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyTuple_Check(&self, obj : *mut PyObjectRaw) -> c_long {
     RPyTuple_Check(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyInt_Check(&self, obj : *mut PyObjectRaw) -> c_long {
     RPyInt_Check(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyString_Check(&self, obj : *mut PyObjectRaw) -> c_long {
     RPyString_Check(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyString_FromString(&self, string : *const c_char) -> *mut PyObjectRaw{
     PyString_FromString(string)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyString_AsString(&self, obj: *mut PyObjectRaw) -> *const c_char {
     PyString_AsString(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyObject_GetAttrString(&self, object : *mut PyObjectRaw, attr : *const c_char) -> *mut PyObjectRaw {
     PyObject_GetAttrString(object, attr)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyErr_Fetch(&self, ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw) {
     PyErr_Fetch(ptype, pvalue, ptraceback);
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyErr_NormalizeException(&self, ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw) {
     PyErr_NormalizeException(ptype, pvalue, ptraceback);
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyObject_Str(&self, obj: *mut PyObjectRaw) -> *mut PyObjectRaw {
     PyObject_Str(obj)
   }
-  #[allow(non_snake_case_functions)]
   unsafe fn PyObject_CallObject(&self, callable_object : *mut PyObjectRaw, args :*mut PyObjectRaw) -> *mut PyObjectRaw {
     PyObject_CallObject(callable_object, args)
   }
