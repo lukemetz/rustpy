8169444a03080376043128e447a76f4b4a70dd34
Yohai Berreby (yberreby) <yohaiberreby@gmail.com>
2016-03-19 19:53:26 +0100

    run rustfmt

--- src/ffi.rs
+++ src/ffi.rs
@@ -8,160 +8,180 @@ use base::PyState;
 pub struct PyObjectRaw;
 
 #[link(name = "python2.7")]
-extern {
-  fn Py_Initialize();
-  fn Py_Finalize();
+extern "C" {
+    fn Py_Initialize();
+    fn Py_Finalize();
 
-  fn PyImport_ImportModule(name : *const c_char) -> *mut PyObjectRaw;
+    fn PyImport_ImportModule(name: *const c_char) -> *mut PyObjectRaw;
 
-  fn Py_DecRef(obj: *mut PyObjectRaw);
+    fn Py_DecRef(obj: *mut PyObjectRaw);
 
-  fn PyObject_CallObject(callable_object : *mut PyObjectRaw, args :*mut PyObjectRaw) -> *mut PyObjectRaw;
-  fn PyObject_GetAttrString(object : *mut PyObjectRaw, attr : *const c_char) -> *mut PyObjectRaw;
-  fn PyObject_Str(obj: *mut PyObjectRaw) -> *mut PyObjectRaw;
-  fn PyObject_GetIter(obj : *mut PyObjectRaw) -> *mut PyObjectRaw;
+    fn PyObject_CallObject(callable_object: *mut PyObjectRaw,
+                           args: *mut PyObjectRaw)
+                           -> *mut PyObjectRaw;
+    fn PyObject_GetAttrString(object: *mut PyObjectRaw, attr: *const c_char) -> *mut PyObjectRaw;
+    fn PyObject_Str(obj: *mut PyObjectRaw) -> *mut PyObjectRaw;
+    fn PyObject_GetIter(obj: *mut PyObjectRaw) -> *mut PyObjectRaw;
 
-  fn PyInt_FromLong(ival : c_long) -> *mut PyObjectRaw;
-  fn PyInt_AsLong(obj : *mut PyObjectRaw) -> c_long;
+    fn PyInt_FromLong(ival: c_long) -> *mut PyObjectRaw;
+    fn PyInt_AsLong(obj: *mut PyObjectRaw) -> c_long;
 
-  fn PyFloat_FromDouble(value : c_double) -> *mut PyObjectRaw;
-  fn PyFloat_AsDouble(obj : *mut PyObjectRaw) -> c_double;
+    fn PyFloat_FromDouble(value: c_double) -> *mut PyObjectRaw;
+    fn PyFloat_AsDouble(obj: *mut PyObjectRaw) -> c_double;
 
-  fn PyTuple_New(size : size_t) -> *mut PyObjectRaw;
-  fn PyTuple_GetItem(tuple : *mut PyObjectRaw, pos : size_t) -> *mut PyObjectRaw;
-  fn PyTuple_SetItem(tuple : *mut PyObjectRaw, pos : size_t, o : *mut PyObjectRaw);
-  fn PyTuple_Size(tuple : *mut PyObjectRaw) -> c_long;
+    fn PyTuple_New(size: size_t) -> *mut PyObjectRaw;
+    fn PyTuple_GetItem(tuple: *mut PyObjectRaw, pos: size_t) -> *mut PyObjectRaw;
+    fn PyTuple_SetItem(tuple: *mut PyObjectRaw, pos: size_t, o: *mut PyObjectRaw);
+    fn PyTuple_Size(tuple: *mut PyObjectRaw) -> c_long;
 
-  fn PyList_New(size : size_t) -> *mut PyObjectRaw;
-  fn PyList_GetItem(list : *mut PyObjectRaw, index : size_t) -> *mut PyObjectRaw;
-  fn PyList_SetItem(list : *mut PyObjectRaw, index : size_t, item : *mut PyObjectRaw);
-  fn PyList_Size(list : *mut PyObjectRaw) -> c_long;
+    fn PyList_New(size: size_t) -> *mut PyObjectRaw;
+    fn PyList_GetItem(list: *mut PyObjectRaw, index: size_t) -> *mut PyObjectRaw;
+    fn PyList_SetItem(list: *mut PyObjectRaw, index: size_t, item: *mut PyObjectRaw);
+    fn PyList_Size(list: *mut PyObjectRaw) -> c_long;
 
-  fn PyString_FromString(string : *const c_char) -> *mut PyObjectRaw;
-  fn PyString_AsString(obj: *mut PyObjectRaw) -> *const c_char;
+    fn PyString_FromString(string: *const c_char) -> *mut PyObjectRaw;
+    fn PyString_AsString(obj: *mut PyObjectRaw) -> *const c_char;
 
-  fn Py_IncRef(obj: *mut PyObjectRaw);
+    fn Py_IncRef(obj: *mut PyObjectRaw);
 
-  fn PyErr_Fetch(ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw);
-  fn PyErr_NormalizeException(ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw);
+    fn PyErr_Fetch(ptype: *mut *mut PyObjectRaw,
+                   pvalue: *mut *mut PyObjectRaw,
+                   ptraceback: *mut *mut PyObjectRaw);
+    fn PyErr_NormalizeException(ptype: *mut *mut PyObjectRaw,
+                                pvalue: *mut *mut PyObjectRaw,
+                                ptraceback: *mut *mut PyObjectRaw);
 
-  fn PyIter_Next(obj : *mut PyObjectRaw) -> *mut PyObjectRaw;
+    fn PyIter_Next(obj: *mut PyObjectRaw) -> *mut PyObjectRaw;
 }
 
 #[link(name = "python2.7")]
-extern {
-  fn RPyFloat_Check(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyFloat_CheckExact(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyTuple_Check(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyList_Check(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyInt_Check(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyString_Check(obj : *mut PyObjectRaw) -> c_long;
-  fn RPyIter_Check(obj : *mut PyObjectRaw) -> c_long;
+extern "C" {
+    fn RPyFloat_Check(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyFloat_CheckExact(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyTuple_Check(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyList_Check(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyInt_Check(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyString_Check(obj: *mut PyObjectRaw) -> c_long;
+    fn RPyIter_Check(obj: *mut PyObjectRaw) -> c_long;
 }
 
 /// Trait to allow interaction with the python interpreter.
 #[allow(bad_style)]
 pub trait PythonCAPI {
-  unsafe fn Py_Initialize(&self) {
-    Py_Initialize();
-  }
-  unsafe fn Py_Finalize(&self) {
-    Py_Finalize();
-  }
-  unsafe fn PyImport_ImportModule(&self, name : *const c_char) -> *mut PyObjectRaw {
-    PyImport_ImportModule(name)
-  }
-  unsafe fn PyInt_FromLong(&self, ival : c_long) -> *mut PyObjectRaw {
-    PyInt_FromLong(ival)
-  }
-  unsafe fn PyInt_AsLong(&self, obj : *mut PyObjectRaw) -> c_long {
-    PyInt_AsLong(obj)
-  }
-  unsafe fn PyFloat_FromDouble(&self, value : c_double) -> *mut PyObjectRaw {
-    PyFloat_FromDouble(value)
-  }
-  unsafe fn PyFloat_AsDouble(&self, obj : *mut PyObjectRaw) -> c_double {
-    PyFloat_AsDouble(obj)
-  }
-  unsafe fn PyTuple_New(&self, size : size_t) -> *mut PyObjectRaw {
-    PyTuple_New(size)
-  }
-  unsafe fn PyTuple_GetItem(&self, tuple : *mut PyObjectRaw, pos : size_t) -> *mut PyObjectRaw {
-    PyTuple_GetItem(tuple, pos)
-  }
-  unsafe fn PyTuple_SetItem(&self, tuple : *mut PyObjectRaw, pos : size_t, o : *mut PyObjectRaw) {
-    PyTuple_SetItem(tuple, pos, o)
-  }
-  unsafe fn PyTuple_Size(&self, tuple : *mut PyObjectRaw) -> c_long {
-    PyTuple_Size(tuple)
-  }
-  unsafe fn PyList_New(&self, size : size_t) -> *mut PyObjectRaw {
-    PyList_New(size)
-  }
-  unsafe fn PyList_GetItem(&self, list : *mut PyObjectRaw, index : size_t) -> *mut PyObjectRaw {
-    PyList_GetItem(list, index)
-  }
-  unsafe fn PyList_SetItem(&self, list : *mut PyObjectRaw, index : size_t, item: *mut PyObjectRaw) {
-    PyList_SetItem(list, index, item)
-  }
-  unsafe fn PyList_Size(&self, list : *mut PyObjectRaw) -> c_long {
-    PyList_Size(list)
-  }
-  unsafe fn Py_IncRef(&self, obj: *mut PyObjectRaw) {
-    Py_IncRef(obj)
-  }
-  unsafe fn Py_DecRef(&self, obj: *mut PyObjectRaw) {
-    Py_DecRef(obj)
-  }
-  unsafe fn PyFloat_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyFloat_Check(obj)
-  }
-  unsafe fn PyFloat_CheckExact(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyFloat_CheckExact(obj)
-  }
-  unsafe fn PyTuple_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyTuple_Check(obj)
-  }
-  unsafe fn PyList_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyList_Check(obj)
-  }
-  unsafe fn PyInt_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyInt_Check(obj)
-  }
-  unsafe fn PyString_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyString_Check(obj)
-  }
-  unsafe fn PyIter_Check(&self, obj : *mut PyObjectRaw) -> c_long {
-    RPyIter_Check(obj)
-  }
-  unsafe fn PyString_FromString(&self, string : *const c_char) -> *mut PyObjectRaw{
-    PyString_FromString(string)
-  }
-  unsafe fn PyString_AsString(&self, obj: *mut PyObjectRaw) -> *const c_char {
-    PyString_AsString(obj)
-  }
-  unsafe fn PyObject_GetAttrString(&self, object : *mut PyObjectRaw, attr : *const c_char) -> *mut PyObjectRaw {
-    PyObject_GetAttrString(object, attr)
-  }
-  unsafe fn PyErr_Fetch(&self, ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw) {
-    PyErr_Fetch(ptype, pvalue, ptraceback);
-  }
-  unsafe fn PyErr_NormalizeException(&self, ptype : *mut *mut PyObjectRaw, pvalue : *mut *mut PyObjectRaw, ptraceback : *mut *mut PyObjectRaw) {
-    PyErr_NormalizeException(ptype, pvalue, ptraceback);
-  }
-  unsafe fn PyObject_Str(&self, obj: *mut PyObjectRaw) -> *mut PyObjectRaw {
-    PyObject_Str(obj)
-  }
-  unsafe fn PyObject_CallObject(&self, callable_object : *mut PyObjectRaw, args :*mut PyObjectRaw) -> *mut PyObjectRaw {
-    PyObject_CallObject(callable_object, args)
-  }
-  unsafe fn PyObject_GetIter(&self, obj : *mut PyObjectRaw) -> *mut PyObjectRaw {
-    PyObject_GetIter(obj)
-  }
-  unsafe fn PyIter_Next(&self, obj : *mut PyObjectRaw) -> *mut PyObjectRaw {
-    PyIter_Next(obj)
-  }
+    unsafe fn Py_Initialize(&self) {
+        Py_Initialize();
+    }
+    unsafe fn Py_Finalize(&self) {
+        Py_Finalize();
+    }
+    unsafe fn PyImport_ImportModule(&self, name: *const c_char) -> *mut PyObjectRaw {
+        PyImport_ImportModule(name)
+    }
+    unsafe fn PyInt_FromLong(&self, ival: c_long) -> *mut PyObjectRaw {
+        PyInt_FromLong(ival)
+    }
+    unsafe fn PyInt_AsLong(&self, obj: *mut PyObjectRaw) -> c_long {
+        PyInt_AsLong(obj)
+    }
+    unsafe fn PyFloat_FromDouble(&self, value: c_double) -> *mut PyObjectRaw {
+        PyFloat_FromDouble(value)
+    }
+    unsafe fn PyFloat_AsDouble(&self, obj: *mut PyObjectRaw) -> c_double {
+        PyFloat_AsDouble(obj)
+    }
+    unsafe fn PyTuple_New(&self, size: size_t) -> *mut PyObjectRaw {
+        PyTuple_New(size)
+    }
+    unsafe fn PyTuple_GetItem(&self, tuple: *mut PyObjectRaw, pos: size_t) -> *mut PyObjectRaw {
+        PyTuple_GetItem(tuple, pos)
+    }
+    unsafe fn PyTuple_SetItem(&self, tuple: *mut PyObjectRaw, pos: size_t, o: *mut PyObjectRaw) {
+        PyTuple_SetItem(tuple, pos, o)
+    }
+    unsafe fn PyTuple_Size(&self, tuple: *mut PyObjectRaw) -> c_long {
+        PyTuple_Size(tuple)
+    }
+    unsafe fn PyList_New(&self, size: size_t) -> *mut PyObjectRaw {
+        PyList_New(size)
+    }
+    unsafe fn PyList_GetItem(&self, list: *mut PyObjectRaw, index: size_t) -> *mut PyObjectRaw {
+        PyList_GetItem(list, index)
+    }
+    unsafe fn PyList_SetItem(&self,
+                             list: *mut PyObjectRaw,
+                             index: size_t,
+                             item: *mut PyObjectRaw) {
+        PyList_SetItem(list, index, item)
+    }
+    unsafe fn PyList_Size(&self, list: *mut PyObjectRaw) -> c_long {
+        PyList_Size(list)
+    }
+    unsafe fn Py_IncRef(&self, obj: *mut PyObjectRaw) {
+        Py_IncRef(obj)
+    }
+    unsafe fn Py_DecRef(&self, obj: *mut PyObjectRaw) {
+        Py_DecRef(obj)
+    }
+    unsafe fn PyFloat_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyFloat_Check(obj)
+    }
+    unsafe fn PyFloat_CheckExact(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyFloat_CheckExact(obj)
+    }
+    unsafe fn PyTuple_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyTuple_Check(obj)
+    }
+    unsafe fn PyList_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyList_Check(obj)
+    }
+    unsafe fn PyInt_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyInt_Check(obj)
+    }
+    unsafe fn PyString_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyString_Check(obj)
+    }
+    unsafe fn PyIter_Check(&self, obj: *mut PyObjectRaw) -> c_long {
+        RPyIter_Check(obj)
+    }
+    unsafe fn PyString_FromString(&self, string: *const c_char) -> *mut PyObjectRaw {
+        PyString_FromString(string)
+    }
+    unsafe fn PyString_AsString(&self, obj: *mut PyObjectRaw) -> *const c_char {
+        PyString_AsString(obj)
+    }
+    unsafe fn PyObject_GetAttrString(&self,
+                                     object: *mut PyObjectRaw,
+                                     attr: *const c_char)
+                                     -> *mut PyObjectRaw {
+        PyObject_GetAttrString(object, attr)
+    }
+    unsafe fn PyErr_Fetch(&self,
+                          ptype: *mut *mut PyObjectRaw,
+                          pvalue: *mut *mut PyObjectRaw,
+                          ptraceback: *mut *mut PyObjectRaw) {
+        PyErr_Fetch(ptype, pvalue, ptraceback);
+    }
+    unsafe fn PyErr_NormalizeException(&self,
+                                       ptype: *mut *mut PyObjectRaw,
+                                       pvalue: *mut *mut PyObjectRaw,
+                                       ptraceback: *mut *mut PyObjectRaw) {
+        PyErr_NormalizeException(ptype, pvalue, ptraceback);
+    }
+    unsafe fn PyObject_Str(&self, obj: *mut PyObjectRaw) -> *mut PyObjectRaw {
+        PyObject_Str(obj)
+    }
+    unsafe fn PyObject_CallObject(&self,
+                                  callable_object: *mut PyObjectRaw,
+                                  args: *mut PyObjectRaw)
+                                  -> *mut PyObjectRaw {
+        PyObject_CallObject(callable_object, args)
+    }
+    unsafe fn PyObject_GetIter(&self, obj: *mut PyObjectRaw) -> *mut PyObjectRaw {
+        PyObject_GetIter(obj)
+    }
+    unsafe fn PyIter_Next(&self, obj: *mut PyObjectRaw) -> *mut PyObjectRaw {
+        PyIter_Next(obj)
+    }
 }
 
-impl PythonCAPI for PyState {
-}
+impl PythonCAPI for PyState {}
--- src/rustpy.rs
+++ src/rustpy.rs
@@ -1,38 +1,38 @@
-/*!
-A simple library to allow for easy use of python from rust.
-
-This library is meant to be middle ware for users wanting to use
-python libraries from rust. It allows users who want to quickly use exciting
-tools, at the price of speed, and to get going fast.
-Originally it was intended to bootstrap machine learning for rust.
-
-It provides a way to interact
-with a python interpreter, via [`PyState`](struct.PyState.html) as well as quick conversion
-from rust types to python types via the [`ToPyType`](trait.ToPyType.html) and
-[`FromPyType`](trait.FromPyType.html) traits.
-
-
-```rust
-extern crate rustpy;
-use rustpy::{ToPyType, FromPyType, PyState};
-
-
-fn main() {
-  let py = PyState::new();
-  let module = py.get_module("math").unwrap();
-  let func = module.get_func("sqrt").unwrap();
-  let v = (144f32, );
-  let args = v.to_py_object(&py).unwrap();
-  let untyped_res = func.call(&args).unwrap();
-  let result = py.from_py_object::<f32>(untyped_res).unwrap();
-  assert_eq!(result, 12f32);
-}
-```
-*/
+//! A simple library to allow for easy use of python from rust.
+//!
+//! This library is meant to be middle ware for users wanting to use
+//! python libraries from rust. It allows users who want to quickly use exciting
+//! tools, at the price of speed, and to get going fast.
+//! Originally it was intended to bootstrap machine learning for rust.
+//!
+//! It provides a way to interact
+//! with a python interpreter, via [`PyState`](struct.PyState.html) as well as quick conversion
+//! from rust types to python types via the [`ToPyType`](trait.ToPyType.html) and
+//! [`FromPyType`](trait.FromPyType.html) traits.
+//!
+//!
+//! ```rust
+//! extern crate rustpy;
+//! use rustpy::{ToPyType, FromPyType, PyState};
+//!
+//!
+//! fn main() {
+//! let py = PyState::new();
+//! let module = py.get_module("math").unwrap();
+//! let func = module.get_func("sqrt").unwrap();
+//! let v = (144f32, );
+//! let args = v.to_py_object(&py).unwrap();
+//! let untyped_res = func.call(&args).unwrap();
+//! let result = py.from_py_object::<f32>(untyped_res).unwrap();
+//! assert_eq!(result, 12f32);
+//! }
+//! ```
+//!
 #![crate_type = "lib"]
 
 extern crate libc;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 
 pub use base::{ToPyType, FromPyType, PyState, PyObject, PyObjectRaw, PyError, PyIterator};
 pub use primtypes::NoArgs;
